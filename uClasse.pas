c3b30fd8ceda238ea74c2e2ced851bde0213911c
Brunozzc0802 <brunotesser2019@gmail.com>
2025-07-28 17:00:35 -0300

    Adição de Classe e Main

--- uClasse.pas
+++ uClasse.pas
@@ -2,6 +2,41 @@ unit uClasse;
 
 interface
 
+  //Abstração\\
+  //Encapsulamento\\
+  type TPersonagem = class (TObject)
+    Nome:String;
+    Vida:Integer;
+    Genero:String;
+    Nivel:Integer;
+    Dano:Integer;
+    Defesa:Integer;
+
+    constructor Create (aDano,aDefesa:Integer);
+
+  end;
+
+  //herença\\
+  type TPlayer = class (TPersonagem)
+    NickName:String;
+    Experiencia:double;
+  end;
+
+  //boss\\
+  type TBoss = class (TPersonagem)
+
+  end;
+
+
 implementation
+
+{ TPersonagem }
+
+constructor TPersonagem.Create(aDano, aDefesa: Integer);
+
+  begin
+    Dano := aDano;
+    Defesa := aDefesa;
+  end;
 
 end.
